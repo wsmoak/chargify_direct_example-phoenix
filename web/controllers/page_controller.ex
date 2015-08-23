050f3e5b39c40f762db5664aa3484c755821449b
Wendy Smoak <wsmoak@gmail.com>
2015-08-22 22:11:47 -0400

    Add a thanks page. Rename the signature function.

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,16 +1,28 @@
 defmodule ChargifyDirectExample.PageController do
   use ChargifyDirectExample.Web, :controller
 
+  def callback(conn, %{"result_code" => "2000"}) do
+    conn
+    |> render("thanks.html")
+  end
+
+  # if the result code wasn't 2000 then we need to redisplay the form
+  def callback(conn, params) do
+    # TODO: display the errors
+    index(conn, params)
+  end
+
   def index(conn, _params) do
     conn
     |> assign(:api_id, api_id)
     |> assign(:timestamp, timestamp)
     |> assign(:nonce, nonce)
     |> assign(:secure_data, secure_data)
-    |> assign_signature
+    |> assign_secure_signature
     |> render("index.html")
   end
 
+  # https://docs.chargify.com/chargify-direct-introduction#secure-parameters-signature
   # http://stackoverflow.com/questions/27082396/how-does-one-generate-an-hmac-string-in-elixir
   # http://www.erlang.org/doc/man/crypto.html
   defp secure_signature(document) do
@@ -19,8 +31,9 @@ defmodule ChargifyDirectExample.PageController do
     |> String.downcase
   end
 
-  defp assign_signature(conn) do
+  defp assign_secure_signature(conn) do
     document = conn.assigns.api_id <> to_string(conn.assigns.timestamp) <> conn.assigns.nonce <> conn.assigns.secure_data
+
     assign(conn, :secure_signature, secure_signature(document) )
   end
 
@@ -30,7 +43,7 @@ defmodule ChargifyDirectExample.PageController do
   end
 
   defp secure_data do
-    "redirect_uri=http%3A%2F%2Flocalhost%3A4000"
+    "redirect_uri=http%3A%2F%2Flocalhost%3A4000%2Fcallback"
   end
 
   defp api_id do
