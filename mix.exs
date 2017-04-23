dad19aba1ee5c6b06f7fadd8977f0ebc17a68170
Wendy Smoak <wsmoak@gmail.com>
2017-04-22 21:33:44 -0400

    Lock Phoenix to 1.2.1 and update related dependencies

--- mix.exs
+++ mix.exs
@@ -29,10 +29,10 @@ defmodule ChargifyDirectExample.Mixfile do
   #
   # Type `mix help deps` for examples and options
   defp deps do
-    [{:phoenix, github: "phoenixframework/phoenix", override: true},
-     {:phoenix_ecto, "~> 1.1"},
+    [{:phoenix, "1.2.1"},
+     {:phoenix_ecto, "2.0.3"},
      {:postgrex, ">= 0.0.0"},
-     {:phoenix_html, "~> 2.1.2"},
+     {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:cowboy, "~> 1.0"},
      {:uuid, "1.0.1" },
